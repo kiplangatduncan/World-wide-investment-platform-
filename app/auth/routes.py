f3393e3d92868699ddd4bcc4de4deb5cbfabeda4
kiplangatduncan <kiruiduncan986@gmail.com>
2026-09-17 09:22:47 +0300

    Update routes.py

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -129,7 +129,7 @@ def login():
             if next_page:
                 return redirect(next_page)
 
-            return redirect(url_for("dashboard"))
+            return redirect(url_for("main.dashboard"))
 
         flash(
             "Invalid username/email or password.",
